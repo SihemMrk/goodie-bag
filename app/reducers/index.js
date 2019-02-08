8d636666e34c58b3b5bacd94ef4240aa0061707e
Sihem Meriki <sihemmeriki2@gmail.com>
2019-02-08 16:11:30 -0500

    candiesList

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,11 +1,17 @@
-
-const initialState = {}
+const initialState = {
+  candies: []
+};
+const MANAGE_CANDIES = "MANAGE_CANDIES";
 
 const rootReducer = (state = initialState, action) => {
+  let newState = { ...state };
   switch (action.type) {
+    case MANAGE_CANDIES: {
+      newState.candies.push({ candies: action.candies });
+    }
     default:
-      return state
+      return state;
   }
-}
+};
 
-export default rootReducer
+export default rootReducer;
--- server/api/index.js
+++ server/api/index.js
@@ -1,7 +1,18 @@
-'use strict'
+"use strict";
 
-const router = require('express').Router()
+const router = require("express").Router();
+const Candy = require("../db/models/Candy");
 
+router.use("Candies", require("../db/models/Candy"));
+
+router.get("/", async (req, res, next) => {
+  try {
+    const candies = await Candies.findAll();
+    res.json(candies);
+  } catch (error) {
+    next(error);
+  }
+});
 // Your routes go here!
 // NOTE: Any routes that you put here are ALREADY mounted on `/api`
 // You can put all routes in this file HOWEVER,
@@ -19,9 +30,9 @@ const router = require('express').Router()
 // middleware will generate a 404, and send it to your
 // error-handling endware!
 router.use((req, res, next) => {
-  const err = new Error('API route not found!')
-  err.status = 404
-  next(err)
-})
+  const err = new Error("API route not found!");
+  err.status = 404;
+  next(err);
+});
 
-module.exports = router
+module.exports = router;
